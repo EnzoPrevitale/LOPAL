001412bcb4df2d4342647e49b52127ad583b93b3
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-03-22 23:57:37 -0300

    Lista de exercícios completa - Condicionais e Teste de Mesa

--- condicionais_e_teste_de_mesa.py
+++ condicionais_e_teste_de_mesa.py
@@ -1,6 +1,6 @@
-# Condicionais
+### CONDICIONAIS ###
 
-# 01
+# 01 - Ano bissexto
 '''
 # Solicitando um ano ao usuário
 ano = int(input("Digite um ano: "))
@@ -17,7 +17,7 @@ if bissexto:
 else:
     print("O ano não é bissexto.")
 '''
-# 02
+# 02 - Calculadora de IMC
 '''
 # Solicitando o peso e a altura do usuário
 peso = float(input("Digite o seu peso, em kg: "))
@@ -41,7 +41,7 @@ else:
 # Imprimindo o IMC e a classificação do usuário
 print(f"O seu IMC é {imc}kg/m². A sua classificação é {classificacao}.")
 '''
-# 03
+# 03 - Calculadora de desconto de produto
 '''
 # Solicitando a quuantidade de produtos e o valor unitário ao usuário
 quantidade_produtos = int(input("Digite a quantidade de produtos a ser adquirida: "))
@@ -61,7 +61,7 @@ valor_descontado = valor_inicial - valor_final
 # Exibindo o valor inicial, o valor final e o valor descontado
 print(f" Valor Unitário: R${valor_unitario} \n Quantidade: {quantidade_produtos} \n Valor inicial: R${valor_inicial} \n Desconto obtido: {desconto*100}% \n Valor descontado: R${valor_descontado} \n Valor final: R${valor_final}")
 '''
-# 04
+# 04 - Verificação de voto obrigatório
 '''
 # Solicitando a idade do usuário
 idade = int(input("Digite a sua idade: "))
@@ -83,7 +83,7 @@ if voto != "Não eleitor":
 else:
     print(f"Você não é elegível para votar.")
 '''
-# 05
+# 05 - Determinar maior e menor idade entre três pessoas
 '''
 # Solicitando as idades das três pessoas
 idade = [int(input("Digite a idade da primeira pessoa: ")), int(input("Digite a idade da segunda pessoa: ")), int(input("Digite a idade da terceira pessoa: "))]
@@ -150,7 +150,7 @@ else:
 # Imprimindo os resultados
 print(f"A maior idade é {maior_idade} e a menor idade é {menor_idade}.")
 '''
-# 06
+# 06 - Verificação de hora válida
 '''
 # Solicitando as horas, os minutos e os segundos
 hora = int(input("Digite a hora: "))
@@ -182,7 +182,7 @@ else:
     # Hora inválida
     print("A hora é inválida.")
 '''
-# 07
+# 07 - Conversão de nota em letra
 '''
 # Solicitando a nota
 nota = float(input("Digite o valor numérico da nota: "))
@@ -213,7 +213,7 @@ else:
 # Imprimindo a nota em letra
 print(f"A nota do aluno é {nota}.")
 '''
-# 08
+# 08 - Verificação de quadrado ou retângulo
 '''
 # Solicitando o valor dos lados
 lado_a = float(input("Digite o valor do lado A: "))
@@ -232,3 +232,90 @@ else:
     # Não retângulo
     print("Os valores digitados não correspondem a um retângulo.")
 '''
+# 09 - Calculadora simples
+'''
+# Solicitando dois números com casas decimais ao usuário
+num1 = float(input("Digite um número com casas decimais: "))
+num2 = float(input("Digite outro número com casas decimais: "))
+
+# Solicitando um comando de operação aritmética ao usuário
+operacao = input("Digite uma operação:\n+ | Adição\n- | Subtração\n* | Multiplicação\n/ | Divisão\n")
+
+# Verificando a operação, realizando-a e armazenando-a na variável resultado
+if operacao == '+':
+    # Adição
+    resultado = num1 + num2
+elif operacao == '-':
+    # Subtração
+    resultado = num1 - num2
+elif operacao == '*':
+    # Multiplicação
+    resultado = num1 * num2
+elif operacao == '/':
+    # Divisão
+    resultado = num1 / num2
+else:
+    # ERRO: Operação desconhecida.
+    resultado = "ERRO: Operação desconhecida."
+
+# Imprimindo a variável resultado
+print(resultado)
+'''
+# 10 - Calculadora de média com descarte de notas
+'''
+# Solicitando três notas ao usuário
+nota1 = float(input("Digite a nota do aluno na primeira prova: "))
+nota2 = float(input("Digite a nota do aluno na segunda prova: "))
+nota3 = float(input("Digite a nota do aluno na terceira prova: "))
+
+# Verificando qual é a menor nota e descartando-a ao alterar o seu valor para 0
+if nota1 < nota2 and nota1 < nota3:
+    # nota1 é a menor
+    nota1 = 0
+elif nota2 < nota1 and nota2 < nota3:
+    # nota2 é a menor
+    nota2 = 0
+elif nota3 < nota1 and nota3 < nota2:
+    # nota3 é a menor
+    nota3 = 0
+elif nota1 == nota2 and nota1 < nota3:
+    # nota1 e nota2 são as menores
+    nota1 = 0
+elif nota1 == nota3 and nota1 < nota2:
+    # nota1 e nota3 são as menores
+    nota1 = 0
+elif nota2 == nota3 and nota2 < nota1:
+    # nota2 e nota3 são as menores
+    nota2 = 0
+elif nota1 == nota2 and nota1 == nota3:
+    # As três notas são iguais
+    nota1 = 0
+
+# Calculando e imprimindo a média entre as notas
+media = (nota1 + nota2 + nota3)/2
+print(media)
+'''
+
+### TESTE DE MESA ###
+
+# 11
+'''
+a = 8
+b = 3
+if a > b:
+    a = a - 2
+b = b + a
+print(a,b)
+'''
+# 12
+
+x = 12
+y = 5
+z = 3
+
+if x > y:
+    z = z + x
+    y = y * 2
+
+x = x - 4
+print(x,y,z)
